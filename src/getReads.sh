98ebc821a43783d4e995eafd948210585b5e57e5
Florian <florian.laubscher@unige.ch>
2022-05-31 11:53:28 +0200

    + get MPXV specifics reads and trimming

---
+++ src/getReads.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+bash fastq2fasta.sh $1
+
+bash blastMP.sh $1
+
+grep -B9 ">" $1_blast.txt | sort | uniq | grep "Query=" | cut -d" " -f2 > $1.reads 
+
+grep -A3 -f $1.reads $1.fastq | sed /^--$/d > $1_MP.fastq   # no empty line in .reads !!
+
+porechop -i $1_MP.fastq -o $1_tr.fastq  # trimming
