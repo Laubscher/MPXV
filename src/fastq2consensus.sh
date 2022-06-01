d9183b03e5f0fb84c20d85029ee407af8c628e1e
Florian <florian.laubscher@unige.ch>
2022-06-01 14:09:50 +0200

    output to log

--- src/fastq2consensus.sh
+++ src/fastq2consensus.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-bash getReads.sh $1 $2
+echo "Starting.."
 
-bash map2MP.sh $1
+echo "logfile; Sample:$1" > $1.log
 
-bash sam2consensus.sh $1
+echo "Getting MPXV specific reads.."
 
-coverage.r $1.depth
+bash getReads.sh $1 $2 2>&1 >>$1.log
+
+echo -e "Found $(wc -l $1.reads)  MPXV specific reads"
+
+rm $1.reads
+
+echo "Map to reference.."
+
+bash map2MP.sh $1 2>&1 >>$1.log
+
+echo "Generate consensus.."
+
+bash sam2consensus.sh $1 2>&1 >>$1.log
+
+echo "Generate coverage plot.."
+
+coverage.r $1.depth 2>&1 >>$1.log
