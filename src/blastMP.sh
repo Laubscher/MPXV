6d4e54145d8b3c44ba98932da64b7d299d36815b
Florian <florian.laubscher@unige.ch>
2022-06-01 11:37:50 +0200

    path to db

--- src/blastMP.sh
+++ src/blastMP.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-blastn -query $1.fasta -db monkeypox/monkeypox.fasta -num_threads 12 -evalue 1 -num_descriptions 1 -num_alignments 1 > $1_blast.txt
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+
+blastn -query $1.fasta -db $SCRIPT_DIR/../db/monkeypox/monkeypox.fasta -num_threads 12 -evalue 1 -num_descriptions 1 -num_alignments 1 > $1_blast.txt
