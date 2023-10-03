ab0b1d188d105269a8d9a90a693bd0a4752ade08
Florian <florian.laubscher@unige.ch>
2023-10-03 11:49:11 +0200

    + crop N at extremity for second consensus don't change the name of the sequence

---
+++ src/cropN2.py
@@ -0,0 +1,30 @@
+# 
+
+import sys
+sequencesDico=dict()
+fasta=open(str(sys.argv[1]), "r")
+for l in fasta:
+  if l[0]==">":
+    AC=l.rstrip().split(">")[1]
+    sequencesDico[AC]=""
+  else: 
+    sequencesDico[AC]+=l.rstrip()        
+ 
+fasta.close()
+
+
+
+for ac in sequencesDico:
+  seq= sequencesDico[ac]
+  try :
+    while seq[0]=="N":
+     seq= seq[1:-1]
+    while seq[-1]=="N":
+     seq= seq[0:-2]
+  
+    print(">"+ ac)  #va remplacé l'ac par nom du fichier càd prélévement + segment
+    print(seq)
+  
+  except :
+    pass
+
