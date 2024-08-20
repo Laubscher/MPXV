509bc4136e5e86b6b928e1bcf99e1eac687251b8
florian <florian.laubscher@outlook.com>
2024-08-20 15:05:38 +0200

    gMPXV-2024

--- src/map2VZV.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
-
-minimap2 -ax map-ont $SCRIPT_DIR/../db/X04370/X04370.fasta $1_tr.fastq > $1.sam
